7a86ac22669c559f12ddfa6cce907db39e994275
Мелега Алексей <melegaalexej@yandex.ru>
2023-03-23 16:27:31 +0300

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿using MyLoginApp.Views;
+namespace MyLoginApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
+	}
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using MyLoginApp.VievModels;
+using MyLoginApp.Views;
+
+namespace MyLoginApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+		builder.Services.AddSingleton<LoginPage>();
+        builder.Services.AddSingleton<ContactPage>();
+        builder.Services.AddSingleton<AboutPage>();
+        builder.Services.AddSingleton<HomePage>();
+
+        builder.Services.AddSingleton<LoginPageViewModel>();
+		return builder.Build();
+	}
+}
